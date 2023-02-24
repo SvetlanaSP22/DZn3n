1d240cb67fe21b384daee888313560378caf3e64
Svetlana <sypkovas@mail.ru>
2023-02-24 21:31:59 +0300

    добавили решение task23

---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите натуральное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 = x;
+string res = $"{x} -> ";
+for (int i = 1; i <= x; i++)
+{
+    int tmp = i * i * i;
+    res += $"{tmp}, ";
+}
+Console.WriteLine(res);
